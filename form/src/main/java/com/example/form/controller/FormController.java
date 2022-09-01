65c3e4bc316d93db3235f32df7e089b614d78125
Roman LUSHKIN <roman.lushkin@avanade.com>
2022-09-01 17:34:00 +0200

    Add css and fix save issues

--- form/src/main/java/com/example/form/controller/FormController.java
+++ form/src/main/java/com/example/form/controller/FormController.java
@@ -1,5 +1,6 @@
 package com.example.form.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,36 +19,59 @@ import com.example.form.repository.UserFeedbackRepository;
 
 @Controller
 public class FormController {
-	
+
 	@Value("${app.title}")
 	private String appTitle;
 
 	@Autowired
 	UserFeedbackRepository repository;
-	
-	
+
 	@GetMapping("/")
-	 public String greetingForm(Model model, OAuth2AuthenticationToken auth) {
-	    model.addAttribute("form", new Form());
-	    model.addAttribute("appTitle", appTitle);
-	    model.addAttribute("userName", auth.getName());
-	    return "form";
-	  }
-	
-	@PostMapping("/save")
-	public String saveForm(@ModelAttribute Form form, Model model, OAuth2AuthenticationToken auth) {
-		OAuth2User principal = auth.getPrincipal();
-		String userId =  principal.getAttribute("oid");
-		UserFeedback userFeedback = this.map(form, userId);
-		UserFeedback savedFeedback = repository.save(userFeedback);
-		
-		model.addAttribute("form",form);
+	public String greetingForm(Model model, OAuth2AuthenticationToken auth) {
+		Iterable<UserFeedback> feedbacks = repository.findAll();
+
+		model.addAttribute("form", new Form());
 		model.addAttribute("appTitle", appTitle);
 		model.addAttribute("userName", auth.getName());
+		model.addAttribute("feedbacks", feedbacks);
+		model.addAttribute("message", null);
 		return "form";
 	}
 
-	private UserFeedback map(Form form, String userId){
+	@PostMapping("/save")
+	public String saveForm(@ModelAttribute Form form, Model model, OAuth2AuthenticationToken auth) {
+		try {
+
+			OAuth2User principal = auth.getPrincipal();
+			String userId = principal.getAttribute("oid");
+			model.addAttribute("appTitle", appTitle);
+			model.addAttribute("userName", auth.getName());
+
+			if (form.getTitle() == null || form.getTitle().isEmpty() || form.getComment() == null
+					|| form.getComment().isEmpty()) {
+				model.addAttribute("form", form);
+				model.addAttribute("message", "Invalid form");
+
+				Iterable<UserFeedback> feedbacks = repository.findAll();
+				model.addAttribute("feedbacks", feedbacks);
+				return "form";
+			} else {
+				model.addAttribute("form", new Form());
+				model.addAttribute("message", null);
+			}
+
+			UserFeedback userFeedback = this.map(form, userId);
+			UserFeedback savedFeedback = repository.save(userFeedback);
+			Iterable<UserFeedback> feedbacks = repository.findAll();
+			model.addAttribute("feedbacks", feedbacks);
+		} catch (Exception ex) {
+			model.addAttribute("message", "Failed to save feedback");
+		}
+
+		return "form";
+	}
+
+	private UserFeedback map(Form form, String userId) {
 		return new UserFeedback(userId, form.getTitle(), form.getComment());
 	}
 
