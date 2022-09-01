d0d13281738110de10992bdd6cba7bf03128e648
Roman LUSHKIN <roman.lushkin@avanade.com>
2022-09-01 14:18:22 +0200

    Clean code

--- form/src/main/java/com/example/form/controller/FormController.java
+++ form/src/main/java/com/example/form/controller/FormController.java
@@ -1,8 +1,6 @@
 package com.example.form.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +9,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.form.model.Form;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class FormController {
-	
-	
-	
-	
 	
 	@Value("${app.title}")
 	private String appTitle;
@@ -40,13 +32,5 @@ public class FormController {
 		model.addAttribute("userName", auth.getName());
 		return "form";
 	}
-	
-	
-	
-
-		
-	
-		  
-	 
 
 }
