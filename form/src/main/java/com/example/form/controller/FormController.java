7b1f252469ee2d7ecbbaf6bb15caa103b17dd8ca
balkisMach <balkis.machlouch@accenture.com>
2022-09-01 09:55:06 +0200

    configuration

--- form/src/main/java/com/example/form/controller/FormController.java
+++ form/src/main/java/com/example/form/controller/FormController.java
@@ -1,5 +1,6 @@
 package com.example.form.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +12,20 @@ import com.example.form.model.Form;
 @Controller
 public class FormController {
 	
+	@Value("${app.title}")
+	private String appTitle;
+	
 	@GetMapping("/")
 	 public String greetingForm(Model model) {
 	    model.addAttribute("form", new Form());
+	    model.addAttribute("appTitle", appTitle);
 	    return "form";
 	  }
 	
 	@PostMapping("/save")
 	public String saveForm(@ModelAttribute Form form, Model model) {
 		model.addAttribute("form",form);
+		model.addAttribute("appTitle", appTitle);
 		return "form";
 	}
 
