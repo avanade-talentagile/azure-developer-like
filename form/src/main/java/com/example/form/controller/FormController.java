1f1b5e54dd33662befbd53123e27d04d93c9b305
balkisMach <balkis.machlouch@accenture.com>
2022-09-01 14:08:51 +0200

    upDate UserNameDisplay

--- form/src/main/java/com/example/form/controller/FormController.java
+++ form/src/main/java/com/example/form/controller/FormController.java
@@ -1,6 +1,8 @@
 package com.example.form.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.form.model.Form;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class FormController {
+	
+	
+	
+	
 	
 	@Value("${app.title}")
 	private String appTitle;
@@ -26,11 +34,15 @@ public class FormController {
 	  }
 	
 	@PostMapping("/save")
-	public String saveForm(@ModelAttribute Form form, Model model) {
+	public String saveForm(@ModelAttribute Form form, Model model, OAuth2AuthenticationToken auth) {
 		model.addAttribute("form",form);
 		model.addAttribute("appTitle", appTitle);
+		model.addAttribute("userName", auth.getName());
 		return "form";
 	}
+	
+	
+	
 
 		
 	
